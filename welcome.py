ae97f6ff045a6e4ea018273d223568ad55fa4dd9
unknown <adenijimujeeb@gmail.com>
2022-05-20 00:22:19 +0100

    feature commit

---
+++ welcome.py
@@ -0,0 +1,29 @@
+print("welcome to vscode!")
+
+
+Files >>> Staging Location >>> Local Repository
+git init
+
+touch repo.py
+code repor.py 
+
+git add repo.py
+git add .
+
+ git commit -m "first commit"
+
+Run globally:
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+Run locally:
+  git config user.email "you@example.com"
+  git config user.name "Your Name"
+
+  git status
+
+  git branch
+  git branch newbranchname
+
+  git checkout newbranchname
